ace45c60ba2f9fac6248266759df46b2f50da131
Saschar <burninges@gmx.de>
2015-03-29 20:46:44 -0400

    FINAL
    Final

--- app/src/main/java/com/example/samona/brainbias/questions/QuestionsBuilder.java
+++ app/src/main/java/com/example/samona/brainbias/questions/QuestionsBuilder.java
@@ -18,11 +18,18 @@ public class QuestionsBuilder {
             , "No", "The phenomena described is the “Gambler’s Fallacy”. It is when people think there odds change based on previous events. E.G. A coin toss is always 50/50."));
         questions.add(new Question("During the coding challenge, Hernan mentioned that creating documents and re-design may be about avoiding the very thing you need to be doing e.g. coding.\nIs this information bias?"
             , "Yes", "The information bias is when people continue to search for information that won’t affect action."));
-        questions.add(new Question("Upon returning to Canada, Mona recalled many details about failing to create a working android ap. Yet she couldn’t remember where the best pisco sours were or, who attended Lais & Brigita’s party.\nIs this a case of too many pisco sours bias?"
+        questions.add(new Question("Upon returning to Canada, Mona recalled many details about failing to create a working android app. Yet she couldn’t remember where the best pisco sours were or, who attended Lais & Brigita’s party.\nIs this a case of too many pisco sours bias?"
             , "No", "This is an example of the negativity bias – where people tend to recall details of nasty events over more pleasant ones."));
         questions.add(new Question("Letícia, Humberto and Márcio seem quiet and introverted therefore they can’t be Brazilian.\nIs this stereotyping?"
             , "Yes", "Stereotyping is expecting people to have specific characteristics based on their membership to a group."));
-
+        questions.add(new Question("After Artem’s presentation on his Intercontinental Ballistic Missile Delivery System (IBMDS), Andreas arranged to have his family move into a bunker.\nIs this the hyperbolic discounting bias in action?"
+            , "No", "When risk is incorrectly estimated it is called the neglecting probability bias, e.g. dying of food poisoning is more likely than dying from a terrorist attack. Note, there is incomplete data on death by IBMDS."));
+        questions.add(new Question("Oddly only men with hair are participating in Hydra II.\nWere male participants chosen based on an observational selection bias?"
+            , "No", "This is an example of insensitivity to sample size.  A.k.a. coming to a conclusion when in fact it may be a coincidence."));
+        questions.add(new Question("Once again, the line-up for Hernan’s coding help grew as he worked with Juliana and Nick. Sascha noticed a pattern and thought it was related to enticing Hernan to sit.\nIs this the Comfortable ass bias?"
+            , "No", "This is an epiphenomena – read Antifragile if you don’t understand."));
+        questions.add(new Question("At the restaurant, exospherians en masse started ordering 3.8 litre beers.\nIs this an example of herd behavior?"
+            , "No", "Herd behavior is often witnessed at riots, sporting events, and during religious frenzies for example. It is often related to groupthink. It could be argued in this scenario it is really about coding week."));
         return questions;
     }
     public Question getRandomQuestion() {
